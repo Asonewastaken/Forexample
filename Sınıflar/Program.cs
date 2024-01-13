130bd9db149aa3b0d069016cff33f94e02420f98
ashaf <ashaf@ASONE>
2024-01-13 19:31:15 +0300

    Add project files.

---
+++ Sınıflar/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sınıflar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Telefon alici = new Telefon("Ashaf", "Şencan", "Iphone", "IOS", 4, 2500);
+            Console.WriteLine("Uygulamama hoşgeldiniz. Lütfen yapmak istediğiniz işlemi seçiniz:");
+            islemleriGoster();
+
+            while (kontrol)
+            {
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+                        alici.KisiselBilgiler();
+                        break;
+
+                    case "2":
+                        alici.TelefonBilgiler();
+                        break;
+
+                    case "3":
+                        Console.Write("Bakiyenizi Giriniz: " );
+                        alici.bakiye = Convert.ToInt32(Console.ReadLine());
+                        alici.Alınabilirlik();
+                        break;
+                        
+                    case "4":
+                        kontrol = false;
+                        break;
+                       
+
+                }
+            }
+        }
+        static void islemleriGoster()
+        {
+            Console.WriteLine("1- Kişisel Bilgileri Göster");
+            Console.WriteLine("2- Telefon Bilgilerini Göster");
+            Console.WriteLine("3- Bütçenizi girin");
+            Console.WriteLine("4- Çıkış yap");
+
+        }
+    }
+}
---
+++ Sınıflar/Telefon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sınıflar
+{
+    public class Telefon
+    {
+        //Parametrelerim
+        public string isim;
+        public string soyisim;
+        public string telefon_markası;
+        public string isletim_sistemi;
+        public int ram_boyutu;
+        public int telefon_fiyatı;
+        public int bakiye;
+
+        //Metotlarım
+        public Telefon(string _isim, string _soyisim, string _telefon_markaası, string _isletim_sistemi, int _ram_boyutu, int _telefon_fiyatı)
+        {
+            isim = _isim;
+            soyisim = _soyisim;
+            telefon_markası = _telefon_markaası;
+            isletim_sistemi = _isletim_sistemi;
+            ram_boyutu = _ram_boyutu;
+            telefon_fiyatı = _telefon_fiyatı;
+        }
+        public void KisiselBilgiler()
+        {
+            Console.WriteLine("İsminiz: " + isim);
+            Console.WriteLine("Soyisminiz: " + soyisim);
+        }
+
+        public void TelefonBilgiler()
+        {
+            Console.WriteLine("Telefonun Markası: " + telefon_markası);
+            Console.WriteLine("Telefonun İşletim Sistemi: " + isletim_sistemi);
+            Console.WriteLine("Telefonun Ram Kapasitesi: " + ram_boyutu);
+            Console.WriteLine("Telefonun Fiyatı: " + telefon_fiyatı);
+        }
+
+        public void Alınabilirlik()
+        {
+            if (bakiye >= telefon_fiyatı)
+            {
+                Console.WriteLine("Bütçeniz bu telefon için uygun. Bu telefonu satın alabilirsiniz.");
+            }
+            else
+            {
+                Console.WriteLine("Bütçeniz bu telefon için uygun değil. Bu telefonu satın alamazsınız.");
+            }
+        }
+    }
+}
